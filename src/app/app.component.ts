9537a7d1424f988e8acb9487f6e336c3dcc995ad
marcospascoski <marcospascoski@gmail.com>
2020-03-10 21:12:09 -0300

    Component Actionsheet, badge e card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,22 @@ export class AppComponent implements OnInit {
       title: 'Alert',
       url: 'alert',
       icon: 'construct'
-    }
+    },
+    {
+      title: 'Actionsheet',
+      url: 'actionsheet',
+      icon: 'construct'
+    },
+    {
+      title: 'Badge',
+      url: 'badge',
+      icon: 'construct'
+    },
+    {
+      title: 'Card',
+      url: 'card',
+      icon: 'construct'
+    },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
